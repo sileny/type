cdcdc6eda7de14796fb48298acd2e4a8c851dfdb
sunsilently <sunsilently@outlook.com>
2020-10-04 01:14:11 +0800

    调整代码顺序

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,16 @@ import { isObject } from "@silen/is-object";
 import { isFunction } from "@silen/is-function";
 
 /**
+ * Determine if a value is a Arguments
+ *
+ * @param {Object} val The value to test
+ * @returns {boolean} True if value is a Arguments, otherwise false
+ */
+function isArguments(val) {
+  return toStr.call(val) === '[object Arguments]';
+}
+
+/**
  * Determine if a value is an Array
  *
  * @param {Object} val The value to test
@@ -13,47 +23,6 @@ function isArray(val) {
 }
 
 /**
- * Determine if a value is undefined
- *
- * @param {Object} val The value to test
- * @returns {boolean} True if the value is undefined, otherwise false
- */
-function isUndefined(val) {
-  return typeof val === 'undefined';
-}
-
-/**
- * Determine if a value is Null
- *
- * @param {Object} val The value to test
- * @returns {boolean} True if the value is Null, otherwise false
- */
-function isNull(val) {
-  return toStr.call(val) === '[object Null]';
-}
-
-/**
- * Determine if a value is Boolean
- *
- * @param {Object} val The value to test
- * @returns {boolean} True if the value is Boolean, otherwise false
- */
-function isBoolean(val) {
-  return toStr.call(val) === '[object Boolean]';
-}
-
-/**
- * Determine if a value is a Buffer
- *
- * @param {Object} val The value to test
- * @returns { Boolean } True if value is a Buffer, otherwise false
- */
-function isBuffer (val) {
-  return !!(val != null && val.constructor != null &&
-    typeof val.constructor.isBuffer === 'function' && val.constructor.isBuffer(val))
-}
-
-/**
  * Determine if a value is an ArrayBuffer
  *
  * @param {Object} val The value to test
@@ -61,16 +30,6 @@ function isBuffer (val) {
  */
 function isArrayBuffer(val) {
   return toStr.call(val) === '[object ArrayBuffer]';
-}
-
-/**
- * Determine if a value is a FormData
- *
- * @param {Object} val The value to test
- * @returns {boolean} True if value is an FormData, otherwise false
- */
-function isFormData(val) {
-  return toStr.call(val) === '[object FormData]';
 }
 
 /**
@@ -99,13 +58,144 @@ function isArrayBufferView(val) {
 }
 
 /**
- * Determine if a value is a String
+ * Determine if a value is a Blob
  *
  * @param {Object} val The value to test
- * @returns {boolean} True if value is a String, otherwise false
+ * @returns {boolean} True if value is a Blob, otherwise false
  */
-function isString(val) {
-  return typeof val === 'string';
+function isBlob(val) {
+  return toStr.call(val) === '[object Blob]';
+}
+
+/**
+ * Determine if a value is Boolean
+ *
+ * @param {Object} val The value to test
+ * @returns {boolean} True if the value is Boolean, otherwise false
+ */
+function isBoolean(val) {
+  return toStr.call(val) === '[object Boolean]';
+}
+
+/**
+ * Determine if a value is a Buffer
+ *
+ * @param {Object} val The value to test
+ * @returns { Boolean } True if value is a Buffer, otherwise false
+ */
+function isBuffer (val) {
+  return !!(val != null && val.constructor != null &&
+    typeof val.constructor.isBuffer === 'function' && val.constructor.isBuffer(val))
+}
+
+/**
+ * Determine if a value is a Date
+ *
+ * @param {Object} val The value to test
+ * @returns {boolean} True if value is a Date, otherwise false
+ */
+function isDate(val) {
+  return toStr.call(val) === '[object Date]';
+}
+
+/**
+ * Determine if a value is a Error
+ *
+ * @param {Object} val The value to test
+ * @returns {boolean} True if value is a Error, otherwise false
+ */
+function isError(val) {
+  return toStr.call(val) === '[object Error]';
+}
+
+/**
+ * Determine if a value is a File
+ *
+ * @param {Object} val The value to test
+ * @returns {boolean} True if value is a File, otherwise false
+ */
+function isFile(val) {
+  return toStr.call(val) === '[object File]';
+}
+
+/**
+ * Determine if a value is a Float32Array
+ *
+ * @param {Object} val The value to test
+ * @returns {boolean} True if value is a Float32Array, otherwise false
+ */
+function isFloat32Array(val) {
+  return toStr.call(val) === '[object Float32Array]';
+}
+
+/**
+ * Determine if a value is a Float64Array
+ *
+ * @param {Object} val The value to test
+ * @returns {boolean} True if value is a Float64Array, otherwise false
+ */
+function isFloat64Array(val) {
+  return toStr.call(val) === '[object Float64Array]';
+}
+
+/**
+ * Determine if a value is a FormData
+ *
+ * @param {Object} val The value to test
+ * @returns {boolean} True if value is an FormData, otherwise false
+ */
+function isFormData(val) {
+  return toStr.call(val) === '[object FormData]';
+}
+
+/**
+ * Determine if a value is a Int16Array
+ *
+ * @param {Object} val The value to test
+ * @returns {boolean} True if value is a Int16Array, otherwise false
+ */
+function isInt16Array(val) {
+  return toStr.call(val) === '[object Int16Array]';
+}
+
+/**
+ * Determine if a value is a Int32Array
+ *
+ * @param {Object} val The value to test
+ * @returns {boolean} True if value is a Int32Array, otherwise false
+ */
+function isInt32Array(val) {
+  return toStr.call(val) === '[object Int32Array]';
+}
+
+/**
+ * Determine if a value is a Int8Array
+ *
+ * @param {Object} val The value to test
+ * @returns {boolean} True if value is a Int8Array, otherwise false
+ */
+function isInt8Array(val) {
+  return toStr.call(val) === '[object Int8Array]';
+}
+
+/**
+ * Determine if a value is a Map
+ *
+ * @param {Object} val The value to test
+ * @returns {boolean} True if value is a Map, otherwise false
+ */
+function isMap(val) {
+  return toStr.call(val) === '[object Map]';
+}
+
+/**
+ * Determine if a value is Null
+ *
+ * @param {Object} val The value to test
+ * @returns {boolean} True if the value is Null, otherwise false
+ */
+function isNull(val) {
+  return toStr.call(val) === '[object Null]';
 }
 
 /**
@@ -134,53 +224,13 @@ function isPlainObject(val) {
 }
 
 /**
- * Determine if a value is a Date
+ * Determine if a value is a Promise
  *
  * @param {Object} val The value to test
- * @returns {boolean} True if value is a Date, otherwise false
+ * @returns {boolean} True if value is a Promise, otherwise false
  */
-function isDate(val) {
-  return toStr.call(val) === '[object Date]';
-}
-
-/**
- * Determine if a value is a Arguments
- *
- * @param {Object} val The value to test
- * @returns {boolean} True if value is a Arguments, otherwise false
- */
-function isArguments(val) {
-  return toStr.call(val) === '[object Arguments]';
-}
-
-/**
- * Determine if a value is a File
- *
- * @param {Object} val The value to test
- * @returns {boolean} True if value is a File, otherwise false
- */
-function isFile(val) {
-  return toStr.call(val) === '[object File]';
-}
-
-/**
- * Determine if a value is a Blob
- *
- * @param {Object} val The value to test
- * @returns {boolean} True if value is a Blob, otherwise false
- */
-function isBlob(val) {
-  return toStr.call(val) === '[object Blob]';
-}
-
-/**
- * Determine if a value is a Symbol
- *
- * @param {Object} val The value to test
- * @returns {boolean} True if value is a Symbol, otherwise false
- */
-function isSymbol(val) {
-  return toStr.call(val) === '[object Symbol]';
+function isPromise(val) {
+  return toStr.call(val) === '[object Promise]';
 }
 
 /**
@@ -194,13 +244,73 @@ function isRegExp(val) {
 }
 
 /**
- * Determine if a value is a Int8Array
+ * Determine if a value is a Set
  *
  * @param {Object} val The value to test
- * @returns {boolean} True if value is a Int8Array, otherwise false
+ * @returns {boolean} True if value is a Set, otherwise false
  */
-function isInt8Array(val) {
-  return toStr.call(val) === '[object Int8Array]';
+function isSet(val) {
+  return toStr.call(val) === '[object Set]';
+}
+
+/**
+ * Determine if a value is a Stream
+ *
+ * @param {Object} val The value to test
+ * @returns {boolean} True if value is a Stream, otherwise false
+ */
+function isStream(val) {
+  return isObject(val) && isFunction(val.pipe);
+}
+
+/**
+ * Determine if a value is a String
+ *
+ * @param {Object} val The value to test
+ * @returns {boolean} True if value is a String, otherwise false
+ */
+function isString(val) {
+  return typeof val === 'string';
+}
+
+/**
+ * Determine if a value is a Symbol
+ *
+ * @param {Object} val The value to test
+ * @returns {boolean} True if value is a Symbol, otherwise false
+ */
+function isSymbol(val) {
+  return toStr.call(val) === '[object Symbol]';
+}
+
+/**
+ * Determine if a value is a URLSearchParams object
+ *
+ * @param {Object} val The value to test
+ * @returns {boolean} True if value is a URLSearchParams object, otherwise false
+ */
+function isURLSearchParams(val) {
+  return toStr.call(val) === '[object URLSearchParams]';
+}
+
+/**
+ * Determine if a value is a Uint16Array
+ *
+ * @param {Object} val The value to test
+ * @returns {boolean} True if value is a Uint16Array, otherwise false
+ */
+function isUint16Array(val) {
+  return toStr.call(val) === '[object Uint16Array]';
+}
+
+/**
+ * Determine if a value is a Uint32Array
+ *
+ * @param {Object} val The value to test
+ * @returns {boolean} True if value is a Uint32Array, otherwise false
+ */
+function isUint32Array(val) {
+  return toStr.call(val) === '[object Uint32Array]';
 }
 
 /**
@@ -224,113 +334,13 @@ function isUint8ClampedArray(val) {
 }
 
 /**
- * Determine if a value is a Int16Array
+ * Determine if a value is undefined
  *
  * @param {Object} val The value to test
- * @returns {boolean} True if value is a Int16Array, otherwise false
+ * @returns {boolean} True if the value is undefined, otherwise false
  */
-function isInt16Array(val) {
-  return toStr.call(val) === '[object Int16Array]';
-}
-
-/**
- * Determine if a value is a Uint16Array
- *
- * @param {Object} val The value to test
- * @returns {boolean} True if value is a Uint16Array, otherwise false
- */
-function isUint16Array(val) {
-  return toStr.call(val) === '[object Uint16Array]';
-}
-
-/**
- * Determine if a value is a Int32Array
- *
- * @param {Object} val The value to test
- * @returns {boolean} True if value is a Int32Array, otherwise false
- */
-function isInt32Array(val) {
-  return toStr.call(val) === '[object Int32Array]';
-}
-
-/**
- * Determine if a value is a Uint32Array
- *
- * @param {Object} val The value to test
- * @returns {boolean} True if value is a Uint32Array, otherwise false
- */
-function isUint32Array(val) {
-  return toStr.call(val) === '[object Uint32Array]';
-}
-
-/**
- * Determine if a value is a Float32Array
- *
- * @param {Object} val The value to test
- * @returns {boolean} True if value is a Float32Array, otherwise false
- */
-function isFloat32Array(val) {
-  return toStr.call(val) === '[object Float32Array]';
-}
-
-/**
- * Determine if a value is a Float64Array
- *
- * @param {Object} val The value to test
- * @returns {boolean} True if value is a Float64Array, otherwise false
- */
-function isFloat64Array(val) {
-  return toStr.call(val) === '[object Float64Array]';
-}
-
-/**
- * Determine if a value is a Error
- *
- * @param {Object} val The value to test
- * @returns {boolean} True if value is a Error, otherwise false
- */
-function isError(val) {
-  return toStr.call(val) === '[object Error]';
-}
-
-/**
- * Determine if a value is a Promise
- *
- * @param {Object} val The value to test
- * @returns {boolean} True if value is a Promise, otherwise false
- */
-function isPromise(val) {
-  return toStr.call(val) === '[object Promise]';
-}
-
-/**
- * Determine if a value is a Set
- *
- * @param {Object} val The value to test
- * @returns {boolean} True if value is a Set, otherwise false
- */
-function isSet(val) {
-  return toStr.call(val) === '[object Set]';
-}
-
-/**
- * Determine if a value is a WeakSet
- *
- * @param {Object} val The value to test
- * @returns {boolean} True if value is a WeakSet, otherwise false
- */
-function isWeakSet(val) {
-  return toStr.call(val) === '[object WeakSet]';
-}
-
-/**
- * Determine if a value is a Map
- *
- * @param {Object} val The value to test
- * @returns {boolean} True if value is a Map, otherwise false
- */
-function isMap(val) {
-  return toStr.call(val) === '[object Map]';
+function isUndefined(val) {
+  return typeof val === 'undefined';
 }
 
 /**
@@ -344,23 +354,13 @@ function isWeakMap(val) {
 }
 
 /**
- * Determine if a value is a Stream
+ * Determine if a value is a WeakSet
  *
  * @param {Object} val The value to test
- * @returns {boolean} True if value is a Stream, otherwise false
+ * @returns {boolean} True if value is a WeakSet, otherwise false
  */
-function isStream(val) {
-  return isObject(val) && isFunction(val.pipe);
-}
-
-/**
- * Determine if a value is a URLSearchParams object
- *
- * @param {Object} val The value to test
- * @returns {boolean} True if value is a URLSearchParams object, otherwise false
- */
-function isURLSearchParams(val) {
-  return toStr.call(val) === '[object URLSearchParams]';
+function isWeakSet(val) {
+  return toStr.call(val) === '[object WeakSet]';
 }
 
 export {
